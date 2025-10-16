c413e040520bdbd673d3ee87bf544b377c2c1716
Phil Wigner <moldymoose@protonmail.com>
2025-10-15 21:52:30 -0400

    moved DIRECTIVES and INSTRUCTIONS arrays to the functions that search them.  fixed prototype for parse_line to match function call)

--- project1/headers.h
+++ project1/headers.h
@@ -1,3 +1,6 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,12 +15,6 @@ typedef struct symbol {
 } SYMBOL;
 
 typedef SYMBOL* SYMTAB;
-
-const char* DIRECTIVES[] = {"START", "END", "BYTE", "WORD", "RESB", "RESW", "RESR", "EXPORT"};
-
-const char* INSTRUCTIONS[] = {"ADD", "AND", "COMP", "DIV", "J", "JEQ", "JLT", "JSUB", "LDA", "LDCH", "LDL", "LDX", "MUL", "OR", "RD", "RSUB", "STA", "STCH", "STL", "STSW", "STX", "SUB", "TD", "TIX", "WD"};
-
-const char* REQOP[] = {"ADD", "AND", "BYTE", "COMP", "DIV", "EXPORT", "J", "JEQ", "JLT", "JSUB", "LDA", "LDCH", "LDL", "LDX", "MUL", "OR", "RD", "RESB", "RESW", "RESR", "STA", "START", "STCH", "STL", "STSW", "STX", "SUB", "TD", "TIX", "WD" "WORD"};
 
 SYMTAB insert_symbol(SYMTAB table, char name[7], int addr, int srcline);
 
@@ -37,7 +34,7 @@ int get_address(char** line, int* address);
 
 int get_constant(char** line, int* constant, int* size);
 
-int parse_line(SYMTAB* table, char line[1025], int lineNumber, int* locationCounter, int* instructionNumber, int* endFlag);
+int parse_line(SYMTAB* table, char* line, int lineNumber, int* locationCounter, int* instructionNumber, int* endFlag);
 
 void print_error(char* line, int line_number, char* msg);
 
@@ -48,3 +45,5 @@ int get_integer(char** line, int* integer);
 void strip_newline(char* line);
 
 void dangle_free(void** ptr);
+
+#endif
